6e59cb01625d8ccf2e202de9f825d56865584ea7
Ann Maria Jose <ann.jose@streamcaptain.com>
2024-02-01 15:04:56 -0600

    Reorganize and add new variables for random code generation

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,11 +1,13 @@
 /* ------ RANDOM CODES ------ */
 
+var code = ' '; //to store generated codes and initialize to empty value
+var getCode = ' '; //to store entered code
+var btnValue; // for button boolean value
+//create variable to hold the type of characters we want to show as codes
+var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
 //Function to generate combination of characters
 function generateCode() {
-    //Create variables to store generated codes and the type of characters we want to show as codes
-    var code = ' '; //initialize to null value
-    var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
-
     //Generate character multiple times using a loop
     for (i = 1; i <= 8; i++) {
         var char = Math.random() * str.length; //random select a character from the variable and then store in a new variable
